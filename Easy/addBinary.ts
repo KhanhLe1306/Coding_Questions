0ea4489b9b8ba96d134c020268a9b9d1992a2990
Khanh Le <76785054+KhanhLe1306@users.noreply.github.com>
2022-05-29 00:05:07 -0500

    Add files via upload

---
+++ Easy/addBinary.ts
@@ -0,0 +1,78 @@
+function addBinary(a: string, b: string): string {
+	let num1 = +a;
+	console.log(num1);
+	let num2 = +b;
+	console.log(num2);
+	let carryUp = 0;
+	let result = "";
+
+	while (Math.round(num1 / 10) != 0 && Math.round(num2 / 10) != 0) {
+		carryUp += (num1 % 10) + (num2 % 10);
+		if (carryUp == 0 || carryUp == 1) {
+			result += carryUp + "";
+			carryUp = 0;
+		} else if (carryUp == 2) {
+			result += "0";
+			carryUp = 1;
+		} else {
+			result += "1";
+			carryUp = 1;
+		}
+		num1 = Math.round(num1 / 10);
+		num2 = Math.round(num2 / 10);
+		console.log(result);
+	}
+
+	if (Math.round(num1 / 10) == 0 && Math.round(num2 / 10) == 0) {
+		carryUp += num1 + num2;
+		if (carryUp == 0 || carryUp == 1) {
+			result += carryUp + "";
+			carryUp = 0;
+		} else if (carryUp == 2) {
+			result += "0";
+			carryUp = 1;
+		} else {
+			result += "1";
+			carryUp = 1;
+		}
+		result += carryUp + "";
+	} else if (Math.round(num1 / 10) == 0) {
+		while (Math.round(num2 / 10) != 0) {
+			carryUp += num2 % 10;
+			if (carryUp == 0 || carryUp == 1) {
+				result += carryUp + "";
+				carryUp = 0;
+			} else if (carryUp == 2) {
+				result += "0";
+				carryUp = 1;
+			} else {
+				result += "1";
+				carryUp = 1;
+			}
+			num2 = Math.round(num2 / 10);
+			console.log(result);
+		}
+	} else {
+		while (Math.round(num1 / 10) != 0) {
+			carryUp += num1 % 10;
+			if (carryUp == 0 || carryUp == 1) {
+				result += carryUp + "";
+				carryUp = 0;
+			} else if (carryUp == 2) {
+				result += "0";
+				carryUp = 1;
+			} else {
+				result += "1";
+				carryUp = 1;
+			}
+			num1 = Math.round(num1 / 10);
+			console.log(result);
+		}
+	}
+
+	console.log(result);
+
+	return result.split("").reverse().join("");
+}
+
+console.log(addBinary("11", "100"));

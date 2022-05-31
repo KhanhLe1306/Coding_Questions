88be2bd6dd38f3080beb6f02d86a072d9e38f813
Khanh Le <76785054+KhanhLe1306@users.noreply.github.com>
2022-05-30 21:19:26 -0500

    Update addBinary.ts

--- Easy/addBinary.ts
+++ Easy/addBinary.ts
@@ -37,6 +37,19 @@ function addBinary(a: string, b: string): string {
 		}
 		result += carryUp + "";
 	} else if (Math.round(num1 / 10) == 0) {
+            carryUp += num1 % 10 + num2 % 10;
+			if (carryUp == 0 || carryUp == 1) {
+				result += carryUp + "";
+				carryUp = 0;
+			} else if (carryUp == 2) {
+				result += "0";
+				carryUp = 1;
+			} else {
+				result += "1";
+				carryUp = 1;
+			}
+            console.log(result);
+            num2 /= 10;
 		while (Math.round(num2 / 10) != 0) {
 			carryUp += num2 % 10;
 			if (carryUp == 0 || carryUp == 1) {
@@ -52,7 +65,30 @@ function addBinary(a: string, b: string): string {
 			num2 = Math.round(num2 / 10);
 			console.log(result);
 		}
+            carryUp += num2;
+            if (carryUp == 0 || carryUp == 1) {
+				result += carryUp + "";
+				carryUp = 0;
+			} else if (carryUp == 2) {
+				result += "0";
+				carryUp = 1;
+			} else {
+				result += "1";
+				carryUp = 1;
+			}
 	} else {
+            carryUp += num2 % 10 + num1 % 10;
+			if (carryUp == 0 || carryUp == 1) {
+				result += carryUp + "";
+				carryUp = 0;
+			} else if (carryUp == 2) {
+				result += "0";
+				carryUp = 1;
+			} else {
+				result += "1";
+				carryUp = 1;
+			}
+            num1 = Math.round(num1 / 10);
 		while (Math.round(num1 / 10) != 0) {
 			carryUp += num1 % 10;
 			if (carryUp == 0 || carryUp == 1) {
@@ -68,11 +104,23 @@ function addBinary(a: string, b: string): string {
 			num1 = Math.round(num1 / 10);
 			console.log(result);
 		}
+            carryUp += num1;
+            if (carryUp == 0 || carryUp == 1) {
+				result += carryUp + "";
+				carryUp = 0;
+			} else if (carryUp == 2) {
+				result += "0";
+				carryUp = 1;
+			} else {
+				result += "1";
+				carryUp = 1;
+			}
 	}
-
+    if(carryUp != 0) result += carryUp + ''
 	console.log(result);
 
 	return result.split("").reverse().join("");
 }
 
-console.log(addBinary("11", "100"));
+console.log(addBinary("11", "1"));
+  

108a480236d2a2a37b1df4d6ebe463bcde73f529
Khanh Le <khanhle@unomaha.edu>
2023-01-26 21:21:41 -0600

    Set up environment and rewrote HappyNumber

---
+++ Easy/Program/Program.HappyNumber.cs
@@ -0,0 +1,31 @@
+partial class Program{
+    public static bool IsHappy(int n) {
+        List<int> list = new();
+        int k;
+        //int times = 0;
+        while (true)
+        {
+            k = n;
+            int sum = 0;
+            //writeLine(++times);
+            while(n / 10 != 0){
+                sum += (int) Math.Pow(n % 10, 2);
+                n /= 10;
+            }
+            sum += (int) Math.Pow(n % 10, 2);
+            //WriteLine(sum);
+            if(sum == 1) return true;
+            
+            if(list.Contains(k)){
+                return false;
+            }else{
+                list.Add(k);
+            }
+            n = sum;
+            // WriteLine(n);
+            // foreach(var i in list){
+            //     WriteLine(i);
+            // }
+        }
+    }
+}
---
+++ Easy/Program/Program.Helper.cs
@@ -0,0 +1,15 @@
+partial class Program {
+    static void print(string message){
+        WriteLine(message);
+    }
+
+    static void print(int number){
+        WriteLine(number);
+    }
+
+    static void print(bool boolean){
+        WriteLine(boolean);
+    }
+
+
+}
